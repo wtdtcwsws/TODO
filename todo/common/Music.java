c1ce78f41ac9e186042b9fd959db9f933ed02c3f
wutong <602232262@qq.com>
2019-05-21 16:36:10 +0800

    音乐（背景音、音效）控制，开始、暂定、循环播放
    Signed-off-by: wutong <602232262@qq.com>

--- todo/common/Music.java
+++ todo/common/Music.java
@@ -1,63 +1,99 @@
 package com.xuetang9.todo.common;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-import sun.audio.*; //一般用这个头文件会报错
-
+import sun.audio.*; 
+/**
+ * 
+ * @function 实现循环、可暂停、可恢复播放音乐功能
+ * @author 吴桐
+ * @date 2019年5月21日下午2:25:13
+ * @place 公司
+ * @version 1.0.0
+ * @copyright 吴桐
+ */
 public class Music {
-	AudioStream as = null;
-	AudioData ad=null;
-	ContinuousAudioDataStream cads = null;
-	static Scanner input = new Scanner(System.in);
-	
-	
-	public Music (String path) {
-		
-		
+	AudioStream as ;
+
+
+	/**
+	 * @构造方法
+	 * @param path 音乐路径
+	 */
+	public Music(String path) {
+
 		try {
 			this.music(path);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void music(String path) throws IOException {
-		try {
-			java.io.InputStream in = new java.io.FileInputStream(path);
-			as = new sun.audio.AudioStream(in);
-			ad = as.getData();
-		} catch (IOException e) {e.printStackTrace();}
-		//设置循环播放
-		 cads = new ContinuousAudioDataStream(ad);
-		//循环播放开始哦
-		AudioPlayer.player.start(cads);
-		//循环播放停止
-		AudioPlayer.player.stop(cads);
 
+	public void music(String path) throws IOException {
+		// TODO 不能使用try(){}写法，否则会没有声音
+		try{
+			InputStream in = new FileInputStream(path);
+			as = new sun.audio.AudioStream(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void Start() {
-		AudioPlayer.player.start(cads);
+	public void Start(String path) {
+		/*
+		 * 如果当前已经播放，便不再播放
+		 */
+		AudioPlayer.player.start(as);
+		try {
+			if (as.available()>0) {
+				
+				AudioPlayer.player.start(as);
+			}else {
+				java.io.InputStream in = new java.io.FileInputStream(path);
+				as = new sun.audio.AudioStream(in);
+				AudioPlayer.player.start(as);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void Pause() {
-		AudioPlayer.player.stop(cads);
+		AudioPlayer.player.stop(as);
 	}
 
 	public void Continue() {
-		AudioPlayer.player.start(cads);
+		AudioPlayer.player.start(as);
 	}
 
-	public static void main(String[] args) {
-		String path = "resources/sounds/ding.wav";
-		Music ms = new Music(path);
-		ms.Start();
-		System.out.println("按任意键继续");
-		input.nextLine();
-		new Music(path).Start();
-		new Music(path).Start();
-		
-	}
+//	public static void main(String[] args) {
+//		String path = "resources/sounds/kanoon.wav";
+////		String path = "resources/sounds/complete.wav";
+//		Music ms = new Music(path);
+//		ms.Start(path);
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		ms.Pause();
+//		System.out.println("暂停");
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("恢复");
+//		ms.Start(path);
+//		try {
+//			Thread.sleep(10000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("第二首");
+//		ms.Start(path);
+//	}
 
 }
--- todo/ui/view/StartFrame.java
+++ todo/ui/view/StartFrame.java
@@ -12,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import com.xuetang9.todo.common.LayerZIndex;
+import com.xuetang9.todo.common.Music;
 import com.xuetang9.todo.model.TodoTask;
 import com.xuetang9.todo.service.TodoTaskService;
 import com.xuetang9.todo.service.impl.TodoTaskServiceImpl;
@@ -29,6 +30,10 @@ import com.xuetang9.todo.ui.controller.StartMinimizeButtonHandler;
  */
 public class StartFrame extends MyFrame implements ActionListener, Runnable {
 	/**
+	 * 一分钟秒数
+	 */
+	final static int MINUTELENGTH = 1;
+	/**
 	 * 背景面板
 	 */
 	private MyPanel myPanel;
@@ -45,10 +50,6 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 	 */
 	private TaskLabel startLabel;
 	/**
-	 * 创建计时器，并注册当前对象为监视器
-	 */
-//	private Timer timer = new Timer(50, this);
-	/**
 	 * 任务时间
 	 */
 	private int taskSize;
@@ -64,10 +65,35 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 	 * 暂停按钮
 	 */
 	private JButton stopButton;
+	/**
+	 * 运行标识
+	 */
 	private boolean isRunning = false;
+	/**
+	 * 创建任务开始进程
+	 */
 	Thread t1 = new Thread(this);
+	/**
+	 * 创建任务音效路径
+	 */
+	private String dingPath = "resources/sounds/ding.wav";
+	private String breakPath = "resources/sounds/break.wav";
+	private String completePath = "resources/sounds/complete.wav";
+	/**
+	 * 创建任务bgm路径
+	 */
+	private String bgm = "resources/sounds/Rain.wav";
+	/**
+	 * 创建bgm音乐对象
+	 */
+	Music bgmMusic = new Music(bgm);
+	/**
+	 * 单例模式
+	 */
+	private static StartFrame single;
 
 	public StartFrame(TaskLabel startLabel) {
+		single = this;
 		this.startLabel = startLabel;
 		this.setSize(400, 300);
 		this.setLocationRelativeTo(null);
@@ -150,6 +176,8 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 			public void actionPerformed(ActionEvent e) {
 				// TODO 不安全，待优化
 				t1.stop();
+				bgmMusic.Pause();
+
 			}
 		});
 		this.addWindowListener(new WindowAdapter() {
@@ -167,42 +195,64 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 
 	@Override
 	public void run() {
-		int minuteLength = 1;
+
+		// 从本地文件得到任务数据
 		TodoTaskService todoTaskService = new TodoTaskServiceImpl();
 		TodoTask startTask = todoTaskService.find(startLabel.getId());
 		taskSize = startTask.getTaskSize();
 		int taskNum = startTask.getTaskNum();
 		int breakSize = startTask.getBreakSize();
 		int longBreakSize = startTask.getLongBreakSize();
-		int breakSecond = breakSize * minuteLength;
-		int longBreakSecond = longBreakSize * minuteLength;
-		int runSecond = taskSize * minuteLength;
-		/**
-		 * 轮次
-		 */
+		// 休息的秒数
+		int breakSecond = breakSize * MINUTELENGTH;
+		// 长休的秒数
+		int longBreakSecond = longBreakSize * MINUTELENGTH;
+		// 任务秒数
+		int taskSecond = taskSize * MINUTELENGTH;
+		// 记录当前轮次
 		int rounds = 1;
+
 		while (taskNum > 0) {
-			System.out.printf("正在进行第%s轮，还剩%s轮%n",rounds,taskNum-1);
-			while (runSecond >= 0 && taskNum > 0) {
+			System.out.printf("正在进行第%s轮，还剩%s轮%n", rounds, taskNum - 1);
+
+			// 计时中
+			while (taskSecond >= 0 && taskNum > 0) {
 				try {
 					if (!isRunning) {
 						synchronized (this) {
 							this.wait();
 						}
 					}
-					int mint = runSecond / 60;
-					int sec = runSecond % 60;
+					// 任务开始，播放背景音
+					bgmMusic.Start(bgm);
+
+					int mint = taskSecond / 60;
+					int sec = taskSecond % 60;
 
 					String timeformat = String.format("计时中%s:%s", mint, sec);
 					countDownLabel.setText(timeformat);
+					// 每一秒刷新
 					Thread.sleep(1000);
-					runSecond--;
+					taskSecond--;
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			breakSecond = breakSize * minuteLength;
-			longBreakSecond = longBreakSize * minuteLength;
+			// 单任务完成音效
+			try {
+				Music ding = new Music(dingPath);
+				// bgm暂停
+				bgmMusic.Pause();
+				ding.Start(dingPath);
+				// 暂停2秒
+				Thread.sleep(2000);
+				ding.Pause();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			breakSecond = breakSize * MINUTELENGTH;
+			longBreakSecond = longBreakSize * MINUTELENGTH;
+			// 休息中，包括长休时间
 			while (breakSecond >= 0 && taskNum > 0 && longBreakSecond >= 0) {
 				switch (rounds % 3) {
 				case 0:
@@ -219,7 +269,7 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 						countDownLabel.setText(timeformat);
 						Thread.sleep(1000);
 						longBreakSecond--;
-						if (longBreakSecond==0) {
+						if (longBreakSecond == 0) {
 							continue;
 						}
 					} catch (InterruptedException e) {
@@ -241,7 +291,7 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 						countDownLabel.setText(timeformat);
 						Thread.sleep(1000);
 						breakSecond--;
-						if (breakSecond==0) {
+						if (breakSecond == 0) {
 							continue;
 						}
 					} catch (InterruptedException e) {
@@ -251,12 +301,32 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 				}
 
 			}
+			// 轮次加1
 			rounds++;
-			runSecond = taskSize * minuteLength;
+			// 重置任务时间
+			taskSecond = taskSize * MINUTELENGTH;
+			// 任务数减1
 			taskNum--;
+			// 如果任务数=0更换完成音效
+			if (taskNum == 0) {
+				breakPath = completePath;
+			}
+			try {
+				Music breakDing = new Music(breakPath);
+				breakDing.Start(breakPath);
+				Thread.sleep(1000);
+				breakDing.Pause();
+				
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+
 		}
 	}
 
+	/**
+	 * 计时开关
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == startButton) {
@@ -269,10 +339,38 @@ public class StartFrame extends MyFrame implements ActionListener, Runnable {
 			synchronized (this) {
 				notify();
 				isRunning = false;
+				bgmMusic.Pause();
 			}
 
 		}
 
+	}
+
+	/**
+	 * 得到单例
+	 * 
+	 * @return
+	 */
+	public static StartFrame getInstance() {
+		return single;
+	}
+
+	/**
+	 * 得到当前bgm路径
+	 * 
+	 * @return
+	 */
+	public String getBreakPath() {
+		return breakPath;
+	}
+
+	/**
+	 * 设置当前bgm路径
+	 * 
+	 * @param breakPath
+	 */
+	public void setBreakPath(String breakPath) {
+		this.breakPath = breakPath;
 	}
 
 }
